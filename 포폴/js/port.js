64b8df35cfa9b231ac3155e07c62502cc268e08b
fjwl3361 <fjwl3361@naver.com>
2019-12-11 09:19:01 +0900

    site

---
+++ 포폴/js/port.js
@@ -0,0 +1,86 @@
+
+$('.port-1').click(function(e){
+    e.preventDefault();
+    $('.popup_main1').fadeIn();
+});
+
+$('.popup1>.close1').click(function(e){
+    e.preventDefault();
+    $('.popup_main1').fadeOut();
+});
+
+$('.port-2').click(function(e){
+    e.preventDefault();
+    $('.popup_main2').fadeIn();
+});
+
+$('.popup2>.close2').click(function(e){
+    e.preventDefault();
+    $('.popup_main2').fadeOut();
+});
+
+$('.port-3').click(function(e){
+    e.preventDefault();
+    $('.popup_main3').fadeIn();
+});
+
+$('.popup3>.close3').click(function(e){
+    e.preventDefault();
+    $('.popup_main3').fadeOut();
+});
+$('.port-4').click(function(e){
+    e.preventDefault();
+    $('.popup_main4').fadeIn();
+});
+
+$('.popup4>.close4').click(function(e){
+    e.preventDefault();
+    $('.popup_main4').fadeOut();
+});
+$('.port-5').click(function(e){
+    e.preventDefault();
+    $('.popup_main5').fadeIn();
+});
+
+$('.popup5>.close5').click(function(e){
+    e.preventDefault();
+    $('.popup_main5').fadeOut();
+});
+$('.port-6').click(function(e){
+    e.preventDefault();
+    $('.popup_main6').fadeIn();
+});
+
+$('.popup6>.close6').click(function(e){
+    e.preventDefault();
+    $('.popup_main6').fadeOut();
+});
+$('.port-7').click(function(e){
+    e.preventDefault();
+    $('.popup_main7').fadeIn();
+});
+
+$('.popup7>.close7').click(function(e){
+    e.preventDefault();
+    $('.popup_main7').fadeOut();
+});
+
+$('.port-8').click(function(e){
+    e.preventDefault();
+    $('.popup_main8').fadeIn();
+});
+
+$('.popup8>.close8').click(function(e){
+    e.preventDefault();
+    $('.popup_main8').fadeOut();
+});
+
+$('.port-9').click(function(e){
+    e.preventDefault();
+    $('.popup_main9').fadeIn();
+});
+
+$('.popup9>.close9').click(function(e){
+    e.preventDefault();
+    $('.popup_main9').fadeOut();
+});
